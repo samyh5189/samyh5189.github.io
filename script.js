7eb41fef9d4418a6deb916437a6f6348f809e66a
samyh5189 <9799744@gmail.com>
2024-08-15 15:39:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const EVENTS_DELAY = 20000;
-    const MAX_KEYS_PER_GAME_PER_DAY = 2;
+    const MAX_KEYS_PER_GAME_PER_DAY = 1;
 
     const games = {
         1: {
