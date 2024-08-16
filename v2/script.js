0c5384de05b2273f73c745736ce2ca1604a52d4a
samyh5189 <9799744@gmail.com>
2024-08-16 18:21:37 +0500

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const EVENTS_DELAY = 18000;
-    const MAX_KEYS_PER_GAME_PER_DAY = 1000;
+    const MAX_KEYS_PER_GAME_PER_DAY = 4000;
 
     const games = {
         1: { name: 'Riding Extreme 3D', appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50', promoId: '43e35910-c168-4634-ad4f-52fd764a843f' },
