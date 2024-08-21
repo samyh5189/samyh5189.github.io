06ee6efcfed8598faf694f46e1254da3cb53d154
samyh5189 <9799744@gmail.com>
2024-08-21 14:57:19 +0500

    Update script.js

--- hehe/script.js
+++ hehe/script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
         name: 'Polysphere',
         appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-        eventsDelay: 40000,
+        eventsDelay: 41000,
         attemptsNumber: 10,
          }
     };
