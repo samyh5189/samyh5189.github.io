260f578cb58644f24dd7d6ced30f5e7454e3face
samyh5189 <9799744@gmail.com>
2024-09-04 13:46:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const EVENTS_DELAY = 20000;
-    const MAX_KEYS_PER_GAME_PER_DAY = 1;
+    
+    const MAX_KEYS_PER_GAME_PER_DAY = 1000000;
+    //const EVENTS_DELAY = 20000;
 
     const games = {
         1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+            name: 'ZooPolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            eventsDelay: 21000,
+            attemptsNumber: 22,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+            eventsDelay: 20000,
+            attemptsNumber: 15
         },
         3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+            name: 'Fluff Crusade',
+            appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            eventsDelay: 23000,
+            attemptsNumber: 18,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-        }
+            eventsDelay: 20000,
+            attemptsNumber: 15,
+        },
+        5: {
+            name: 'MergeAway',
+            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+            eventsDelay: 20000,
+            attemptsNumber: 15,
+        },
+        6: {
+            name: 'Twerk Race 3D',
+            appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            eventsDelay: 20000,
+            attemptsNumber: 15,
+            
+        },
+         7 : {
+            name: 'Polysphere',
+            appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+            promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+            eventsDelay: 20000,
+            attemptsNumber: 18,
+         },
+	8: {
+            name: 'Mow and Trim',
+            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            eventsDelay: 21000,
+            attemptsNumber: 17,
+            
+	},
+        9: {
+            name: 'Tile Trio',
+            appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            eventsDelay: 40000,
+            attemptsNumber: 22,
+            
+	},
     };
+
 
     const startBtn = document.getElementById('startBtn');
     const keyCountSelect = document.getElementById('keyCountSelect');
@@ -38,7 +86,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const generatedKeysTitle = document.getElementById('generatedKeysTitle');
     const gameSelect = document.getElementById('gameSelect');
     const copyStatus = document.getElementById('copyStatus');
+    const previousKeysContainer = document.getElementById('previousKeysContainer');
+    const previousKeysList = document.getElementById('previousKeysList');
     const telegramChannelBtn = document.getElementById('telegramChannelBtn');
+
+    //for logs
+    const logMessage = (message) => {
+        const logArea = document.getElementById('logArea');
+        const logCheckbox = document.getElementById('logCheckbox');
+    
+        if (logCheckbox.checked) {
+            logArea.style.display = 'block'; // Show the textarea if logs are enabled
+            logArea.value += message + '\n';
+            logArea.scrollTop = logArea.scrollHeight; // Auto-scroll to the bottom
+        }
+    };
+    
+    document.getElementById('logCheckbox').addEventListener('change', (event) => {
+        const logArea = document.getElementById('logArea');
+        if (event.target.checked) {
+            logArea.style.display = 'block'; // Show the textarea when the checkbox is checked
+        } else {
+            logArea.style.display = 'none';  // Hide the textarea when the checkbox is unchecked
+        }
+    });
 
     const initializeLocalStorage = () => {
         const now = new Date().toISOString().split('T')[0];
@@ -46,7 +117,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const storageKey = `keys_generated_${game.name}`;
             const storedData = JSON.parse(localStorage.getItem(storageKey));
             if (!storedData || storedData.date !== now) {
-                localStorage.setItem(storageKey, JSON.stringify({ date: now, count: 0 }));
+                localStorage.setItem(storageKey, JSON.stringify({ date: now, count: 0, keys: [] }));
             }
         });
     };
@@ -142,7 +213,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const storedData = JSON.parse(localStorage.getItem(storageKey));
 
         if (storedData.count + keyCount > MAX_KEYS_PER_GAME_PER_DAY) {
-            alert(`You can generate only ${MAX_KEYS_PER_GAME_PER_DAY - storedData.count} more keys for ${game.name} today. Please contact 9799744 for more keys.`);
+            alert(`You can generate only ${MAX_KEYS_PER_GAME_PER_DAY - storedData.count} more keys for ${game.name} today.`);
+            previousKeysList.innerHTML = storedData.keys.map(key =>
+                `<div class="key-item">
+                    <input type="text" value="${key}" readonly>
+                </div>`
+            ).join('');
+            previousKeysContainer.classList.remove('hidden');
             return;
         }
 
@@ -150,7 +227,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         progressBar.style.width = '0%';
         progressText.innerText = '0%';
-        progressLog.innerText = 'Starting...';
+        progressLog.innerText = 'Starting... \n Please wait It may take upto 1 min to Login';
         progressContainer.classList.remove('hidden');
         keyContainer.classList.add('hidden');
         generatedKeysTitle.classList.add('hidden');
@@ -168,36 +245,66 @@ document.addEventListener('DOMContentLoaded', () => {
             progressText.innerText = `${progress}%`;
             progressLog.innerText = message;
         };
+    
 
-        const generateKeyProcess = async () => {
-            const clientId = generateClientId();
-            let clientToken;
-            try {
-                clientToken = await login(clientId, game.appToken);
-            } catch (error) {
-                alert(`Failed to login: ${error.message}`);
-                startBtn.disabled = false;
-                return null;
+    const generateKeyProcess = async () => {
+    const clientId = generateClientId();
+    let clientToken;
+    try {
+        clientToken = await login(clientId, game.appToken);
+    } catch (error) {
+        alert(`Failed to login: ${error.message}`);
+        startBtn.disabled = false;
+        return null;
+    }
+    for (let i = 0; i < game.attemptsNumber; i++) {
+        logMessage(`Attempt ${i + 1}: Sending request...`);
+    
+        let countdown = game.eventsDelay / 1000;
+        const countdownContainer = document.getElementById('countdownContainer');
+        const countdownTimer = document.getElementById('countdownTimer');
+    
+        countdownContainer.style.display = 'block';
+        countdownTimer.textContent = countdown;
+    
+        const countdownInterval = setInterval(() => {
+            countdown -= 1;
+            countdownTimer.textContent = countdown;
+    
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
             }
+        }, 1000);
+    
+        await sleep(game.eventsDelay * delayRandom());
+    
+        clearInterval(countdownInterval);
+        countdownContainer.style.display = 'none';
+    
+        const hasCode = await emulateProgress(clientToken, game.promoId);
+        updateProgress(((100 / game.attemptsNumber) / keyCount), 'Emulating progress...');
+    
+        if (hasCode) {
+            logMessage(`Attempt ${i + 1}: Request success. Code received.`);
+            break;
+        } else {
+            logMessage(`Attempt ${i + 1}: Request failed. No code received.`);
+        }
+    }
+    
+    try {
+        logMessage('Generating the key...');
+        const key = await generateKey(clientToken, game.promoId);
+        logMessage('Key generation successful.');
+        updateProgress(30 / keyCount, 'Generating key...');
+        return key;
+    } catch (error) {
+        logMessage(`Key generation failed: ${error.message}`);
+        alert(`Failed to generate key: ${error.message}`);
+        return null;
+    }
+};
 
-            for (let i = 0; i < 11; i++) {
-                await sleep(EVENTS_DELAY * delayRandom());
-                const hasCode = await emulateProgress(clientToken, game.promoId);
-                updateProgress(7 / keyCount, 'Processing...');
-                if (hasCode) {
-                    break;
-                }
-            }
-
-            try {
-                const key = await generateKey(clientToken, game.promoId);
-                updateProgress(30 / keyCount, 'Generating Key...');
-                return key;
-            } catch (error) {
-                alert(`Failed to generate key: ${error.message}`);
-                return null;
-            }
-        };
 
         const keys = await Promise.all(Array.from({ length: keyCount }, generateKeyProcess));
 
@@ -217,7 +324,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>`;
         }
 
-        storedData.count += keyCount;
+        storedData.count += keys.filter(key => key).length;
+        storedData.keys.push(...keys.filter(key => key));
         localStorage.setItem(storageKey, JSON.stringify(storedData));
 
         keyContainer.classList.remove('hidden');
@@ -226,40 +334,81 @@ document.addEventListener('DOMContentLoaded', () => {
             button.addEventListener('click', (event) => {
                 const key = event.target.getAttribute('data-key');
                 navigator.clipboard.writeText(key).then(() => {
-                    copyStatus.classList.remove('hidden');
-                    setTimeout(() => copyStatus.classList.add('hidden'), 2000);
+                    copyStatus.innerText = `Copied ${key}`;
+                    setTimeout(() => {
+                        copyStatus.innerText = '';
+                    }, 2000);
+                }).catch(err => {
+                    console.error('Could not copy text: ', err);
                 });
             });
         });
-        copyAllBtn.addEventListener('click', () => {
-            const keysText = keys.filter(key => key).join('\n');
-            navigator.clipboard.writeText(keysText).then(() => {
-                copyStatus.classList.remove('hidden');
-                setTimeout(() => copyStatus.classList.add('hidden'), 2000);
-            });
+
+        startBtn.disabled = false;
+        keyCountSelect.classList.remove('hidden');
+        gameSelect.classList.remove('hidden');
+        startBtn.classList.remove('hidden');
+    });
+
+    copyAllBtn.addEventListener('click', () => {
+        const allKeys = Array.from(document.querySelectorAll('.key-item input')).map(input => input.value).join('\n');
+        navigator.clipboard.writeText(allKeys).then(() => {
+            copyStatus.innerText = 'All keys copied';
+            setTimeout(() => {
+                copyStatus.innerText = '';
+            }, 2000);
+        }).catch(err => {
+            console.error('Could not copy text: ', err);
+        });
+    });
+
+    document.getElementById('creatorChannelBtn').addEventListener('click', () => {
+        (function() {
+            const part1 = atob('aHR0cHM6Ly90ZWxlZ3JhbS5tZS8=');
+            const part2 = atob('U2FtX0RtX2JvdA==');
+            const url = part1 + part2;
+            window['open'](url, '_blank');
+        })();
+        
+    });
+
+    telegramChannelBtn.addEventListener('click', () => {
+        (function() {
+            const part1 = atob('aHR0cHM6Ly90ZWxlZ3JhbS5tZS8=');
+            const part2 = atob('SW5zdGFfQnV5X0ZvbGxvd2Vy');
+            const url = part1 + part2;
+            window['open'](url, '_blank');
+        })();
+        
+    });
+
+    document.getElementById('ShowKeysBtn').addEventListener('click', () => {
+        const generatedCodesContainer = document.getElementById('generatedCodesContainer');
+        const generatedCodesList = document.getElementById('generatedCodesList');
+        generatedCodesList.innerHTML = ''; // Clear the list
+
+        let codesGeneratedToday = [];
+
+        Object.keys(games).forEach(key => {
+            const game = games[key];
+            const storageKey = `keys_generated_${game.name}`;
+            const storedData = JSON.parse(localStorage.getItem(storageKey));
+
+            if (storedData && storedData.keys && storedData.keys.length > 0) {
+                codesGeneratedToday = codesGeneratedToday.concat(storedData.keys.map(code => {
+                    return `<li>${game.name}: ${code}</li>`;
+                }));
+            }
         });
 
-        progressBar.style.width = '100%';
-        progressText.innerText = '100%';
-        progressLog.innerText = 'Complete';
+        if (codesGeneratedToday.length > 0) {
+            generatedCodesList.innerHTML = codesGeneratedToday.join('');
+        } else {
+            generatedCodesList.innerHTML = '<li>No codes generated today.</li>';
+        }
 
-        startBtn.classList.remove('hidden');
-        keyCountSelect.classList.remove('hidden');
-        gameSelect.classList.remove('hidden');
-        startBtn.disabled = false;
+        generatedCodesContainer.style.display = 'block';
     });
 
-    document.getElementById('generateMoreBtn').addEventListener('click', () => {
-        progressContainer.classList.add('hidden');
-        keyContainer.classList.add('hidden');
-        startBtn.classList.remove('hidden');
-        keyCountSelect.classList.remove('hidden');
-        gameSelect.classList.remove('hidden');
-        generatedKeysTitle.classList.add('hidden');
-        copyAllBtn.classList.add('hidden');
-        keysList.innerHTML = '';
-        keyCountLabel.innerText = 'Number of keys:';
-    });
-
-   
+    
 });
