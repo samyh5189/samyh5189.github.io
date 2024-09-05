a70f87f750ea0e8192303f6ecb3dd7be1d0ff3d8
samyh5189 <9799744@gmail.com>
2024-09-05 21:52:11 +0500

    Update script.js

--- hehe/script.js
+++ hehe/script.js
@@ -70,6 +70,13 @@ document.addEventListener('DOMContentLoaded', () => {
             attemptsNumber: 22,
             
 	},
+	10: {
+            name: 'Stone Age',
+            appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            eventsDelay: 20000,
+            attemptsNumber: 30,
+        }    
     };
 
 
